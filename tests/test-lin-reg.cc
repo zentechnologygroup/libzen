d8703657ec2a4e35a311fa6559216a5608d7118d
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2017-05-02 17:56:24 -0400

    +

--- tests/test-lin-reg.cc
+++ tests/test-lin-reg.cc
@@ -10,7 +10,7 @@ Array<double> y = { 1.1, 2.2, 2.9, 4.05, 4.4, 5, 6.5, 7.9, 9.1, 10.5 };
 int main()
 {
   LinearRegression lm(x, y);
-  lm.train(.01, 20);
+  lm.train(.01, 2000);
   x.for_each([&lm] (auto x)
 	     { cout << "x(" << x << ") = " << lm.predict(x) << endl; });
   cout << "x(0) = " << lm.predict(0) << endl
