4ce464fc09eea4694c868b31734d869c1bcda394
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2017-06-15 09:51:02 -0400

    Lista 1ra versión de grid simple

--- tests/test-simple-grid.cc
+++ tests/test-simple-grid.cc
@@ -1,7 +1,12 @@
 # include <simple-grid.H>
 
-int main(int argc, char *argv[])
-{
-  
+DynList<double> x = { 0, 1, 2, 3, 4, 5 };
+DynList<double> y = { 0, 1, 2, 3, 4, 5 };
 
+int main()
+{
+  SimpleGrid grid(x, y);
+
+  for (double x = 0; x < 5; x += 0.5)
+    cout << "x = " << x << " y = " << grid(x) << endl;
 }
